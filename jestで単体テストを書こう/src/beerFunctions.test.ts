fca05227d1131325b7a38994dcd28807d3cdfb9e
gn-t-k <gntk1995422@gmail.com>
2021-02-02 00:48:14 +0900

    テスト雛形作成

---
+++ jestで単体テストを書こう/src/beerFunctions.test.ts
@@ -0,0 +1,28 @@
+import {
+  getRandomBeer,
+  getRandomBeerList,
+  filterPropertyRandomBeerList,
+} from "./beerFunctions";
+
+/**
+ * Random Data APIの/beer/random_beerからデータを取得する
+ * ドキュメント： https://random-data-api.com/documentation
+ */
+
+describe("getRandomBeer", () => {
+  /**
+   * 単一のBeerオブジェクトを取得する
+   */
+});
+
+describe("getRandomBeerList", () => {
+  /**
+   * 複数のBeerオブジェクトを取得する
+   */
+});
+
+describe("filterPropertyRandomBeerList", () => {
+  /**
+   * 取得した複数のBeerから必要なプロパティだけを抜き出す
+   */
+});
