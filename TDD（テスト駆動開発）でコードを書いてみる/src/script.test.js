c9364ae11bcf384afab16037261c21e2cec9eccc
gn-t-k <gntk1995422@gmail.com>
2021-02-09 22:41:15 +0900

    テスト作成

---
+++ TDD（テスト駆動開発）でコードを書いてみる/src/script.test.js
@@ -0,0 +1,8 @@
+const { TestScheduler } = require("jest");
+const getArguments = require("./get-arguments");
+
+describe("コマンドライン引数から数値を取得できる", () => {
+  test("受け取った文字列を数値に変換できる", () => {
+    expect(getArguments(["1"])).toBe([1]);
+  });
+});
