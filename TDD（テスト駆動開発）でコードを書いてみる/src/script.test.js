f6e4c5336e6b606f55a7da7fbbe35d02e3b6bf10
gn-t-k <gntk1995422@gmail.com>
2021-02-09 23:15:33 +0900

    テスト追加

--- TDD（テスト駆動開発）でコードを書いてみる/src/script.test.js
+++ TDD（テスト駆動開発）でコードを書いてみる/src/script.test.js
@@ -3,6 +3,10 @@ const getArguments = require("./get-arguments");
 
 describe("コマンドライン引数から数値を取得できる", () => {
   test("受け取った文字列を数値に変換できる", () => {
-    expect(getArguments(["1"])).toStrictEqual([1]);
+    expect(getArguments(["1"])).toEqual([1]);
+  });
+
+  test("数値のみ取得する", () => {
+    expect(getArguments(["a"])).toEqual("Only numbers are available");
   });
 });
