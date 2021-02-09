da20a8a0e7d1fc4e26c7c55d6ae77b7a0ff72742
gn-t-k <gntk1995422@gmail.com>
2021-02-09 23:27:57 +0900

    実装

--- TDD（テスト駆動開発）でコードを書いてみる/src/get-arguments.js
+++ TDD（テスト駆動開発）でコードを書いてみる/src/get-arguments.js
@@ -1,1 +1,7 @@
-module.exports = (args) => args.map((arg) => Number(arg));
+module.exports = (args) => {
+  const numbers = args.map((arg) => Number(arg));
+  const numberPattern = /^[+,-]?([1-9]\d*|0)$/;
+  return numbers.every((number) => numberPattern.test(number))
+    ? numbers
+    : "Only numbers are available";
+};
