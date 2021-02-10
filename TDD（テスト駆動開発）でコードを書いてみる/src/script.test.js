621e73cbf888948c3dcfe64e454e0cda5f1e7ecc
gn-t-k <gntk1995422@gmail.com>
2021-02-10 17:26:44 +0900

    関数名変更

--- TDD（テスト駆動開発）でコードを書いてみる/src/script.test.js
+++ TDD（テスト駆動開発）でコードを書いてみる/src/script.test.js
@@ -1,28 +1,28 @@
-const getArguments = require("./get-arguments");
+const getNumbers = require("./get-numbers");
 
 describe("コマンドライン引数から数値を取得できる", () => {
   test("受け取った文字列を数値に変換できる", () => {
-    expect(getArguments(["1"])).toEqual([1]);
+    expect(getNumbers(["1"])).toEqual([1]);
   });
 
   test("数値のみ取得する", () => {
-    expect(getArguments(["a"])).toEqual("Only numbers are available");
+    expect(getNumbers(["a"])).toEqual("Only numbers are available");
   });
 
   test("複数取得できる", () => {
-    expect(getArguments(["1", "2"])).toEqual([1, 2]);
+    expect(getNumbers(["1", "2"])).toEqual([1, 2]);
   });
 
   test("1つでも数値以外のものがあった場合エラーを返す", () => {
-    expect(getArguments(["1", "a", "3"])).toEqual("Only numbers are available");
+    expect(getNumbers(["1", "a", "3"])).toEqual("Only numbers are available");
   });
 
   test("何も渡されなかった場合エラーを返す", () => {
-    expect(getArguments()).toEqual("1 to 30 arguments can be set");
+    expect(getNumbers()).toEqual("1 to 30 arguments can be set");
   });
 
   test("31個以上の引数が渡された場合、エラーを返す", () => {
-    expect(getArguments(Array(31).fill(1))).toEqual(
+    expect(getNumbers(Array(31).fill(1))).toEqual(
       "1 to 30 arguments can be set"
     );
   });
