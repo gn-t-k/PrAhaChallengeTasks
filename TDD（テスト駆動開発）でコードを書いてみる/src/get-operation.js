2ee9dcd0677dc7730784dd8aa1587228392d6d20
gn-t-k <gntk1995422@gmail.com>
2021-02-10 18:33:01 +0900

    実装

--- TDD（テスト駆動開発）でコードを書いてみる/src/get-operation.js
+++ TDD（テスト駆動開発）でコードを書いてみる/src/get-operation.js
@@ -1,1 +1,12 @@
-module.exports = (args) => args?.[0];
+module.exports = (args) => {
+  if (
+    !(
+      args?.[0] === "add" ||
+      args?.[0] === "subtract" ||
+      args?.[0] === "multiply" ||
+      args?.[0] === "devide"
+    )
+  )
+    return "Only add/subtract/multiply/devide is available";
+  return args?.[0];
+};
