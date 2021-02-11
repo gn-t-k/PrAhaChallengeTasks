04da7757c61ed698a86aebf0be4b27c6a0c5c9d0
gn-t-k <gntk1995422@gmail.com>
2021-02-11 17:51:25 +0900

    実装

--- TDD（テスト駆動開発）でコードを書いてみる/src/subtract.js
+++ TDD（テスト駆動開発）でコードを書いてみる/src/subtract.js
@@ -1,1 +1,5 @@
-module.exports = () => {};
+module.exports = (args) => {
+  const base = args[0];
+  const subtraction = args.slice(1).reduce((acc, cur) => acc + cur);
+  return base - subtraction;
+};
