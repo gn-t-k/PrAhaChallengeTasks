b4458d57e2fe14b42a9ac9d08b2565fb348fc5c2
gn-t-k <gntk1995422@gmail.com>
2021-02-11 22:37:47 +0900

    実装

--- TDD（テスト駆動開発）でコードを書いてみる/src/divide.js
+++ TDD（テスト駆動開発）でコードを書いてみる/src/divide.js
@@ -2,5 +2,6 @@ module.exports = (args) => {
   if (args?.length < 2) return "2 or more arguments are required";
   if (args.slice(1).some((arg) => arg === 0))
     return "Division by zero has occurred";
-  return args.reduce((acc, cur) => acc / cur);
+  const result = args.reduce((acc, cur) => acc / cur);
+  return Math.floor(result / 0.01) * 0.01;
 };
