34fb906f2264725fc03a15eea1cb50c0a0c726ca
gn-t-k <gntk1995422@gmail.com>
2021-02-11 18:00:11 +0900

    実装

--- TDD（テスト駆動開発）でコードを書いてみる/src/subtract.js
+++ TDD（テスト駆動開発）でコードを書いてみる/src/subtract.js
@@ -2,5 +2,6 @@ module.exports = (args) => {
   if (args?.length < 2) return "2 or more arguments are required";
   const base = args[0];
   const subtraction = args.slice(1).reduce((acc, cur) => acc + cur);
-  return base - subtraction;
+  const result = base - subtraction;
+  return result >= 0 ? result : "negative number";
 };
