99927cd719ed0e7c21c5cbd072c19264d795e685
gn-t-k <gntk1995422@gmail.com>
2021-02-23 10:43:24 +0900

    ロジック修正

--- TDD（テスト駆動開発）でコードを書いてみる/src/get-operation.js
+++ TDD（テスト駆動開発）でコードを書いてみる/src/get-operation.js
@@ -1,11 +1,9 @@
 module.exports = (args) => {
   if (
-    !(
-      args?.[0] === "add" ||
-      args?.[0] === "subtract" ||
-      args?.[0] === "multiply" ||
-      args?.[0] === "divide"
-    )
+    args?.[0] !== "add" &&
+    args?.[0] !== "subtract" &&
+    args?.[0] !== "multiply" &&
+    args?.[0] !== "divide"
   )
     return "Only add/subtract/multiply/divide is available";
   return args?.[0];
