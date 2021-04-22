f47832ad8d903327cf2ff90b777ac185d36f0d1b
gn-t-k <gntk1995422@gmail.com>
2021-04-23 08:31:44 +0900

    在籍ステータスの値オブジェクト

--- 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/domain/member/value-object/activity-status.ts
+++ 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/domain/member/value-object/activity-status.ts
@@ -1,1 +1,15 @@
-export class ActivityStatus {}
+export class ActivityStatus {
+  private status_: string;
+
+  constructor(status: string) {
+    this.status_ = status;
+  }
+
+  public value(): string {
+    return this.status_;
+  }
+
+  public equals(activityStatus: ActivityStatus): boolean {
+    return activityStatus.value() === this.status_;
+  }
+}
