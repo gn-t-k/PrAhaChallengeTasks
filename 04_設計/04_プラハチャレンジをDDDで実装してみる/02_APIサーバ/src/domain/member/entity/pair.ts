c648458b2df59bce02215c1345ac0c3954df5ae0
gn-t-k <gntk1995422@gmail.com>
2021-04-29 22:24:30 +0900

    [wip]ペアクラス

--- 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/domain/member/entity/pair.ts
+++ 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/domain/member/entity/pair.ts
@@ -1,1 +1,17 @@
-export class Pair {}
+import { Member } from "domain/member/entity/member";
+
+export class Pair {
+  private id_: string;
+  private name_: string;
+  private memberList_: Member[];
+
+  constructor(id: string, name: string, memberList: Member[]) {
+    this.id_ = id;
+    this.name_ = name;
+    this.memberList_ = memberList;
+  }
+
+  public get name(): string {
+    return this.name_;
+  }
+}
--- 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/test/domain/member/entity/pair.test.ts
+++ 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/test/domain/member/entity/pair.test.ts
@@ -1,7 +1,46 @@
+import {
+  Member,
+  IExercise, // TODO: 後で課題オブジェクトに入れ替える
+} from "domain/member/entity/member";
+import { ActivityStatus } from "domain/member/value-object/activity-status";
 import { Pair } from "domain/member/entity/pair";
 
 describe("pair", () => {
   describe("ペアを作成できる", () => {
+    // TODO: ダミーのpropsをmember.testからexportして使う
+    const id = "123";
+    const name = "a";
+    // TODO: 後で課題オブジェクトに入れ替える
+    const exerciseList: IExercise[] = [
+      {
+        title: "課題1",
+        status: "未着手",
+      },
+      {
+        title: "課題2",
+        status: "未着手",
+      },
+    ];
+    const member1 = new Member(
+      "taroid",
+      "taro",
+      "taro@mail.com",
+      new ActivityStatus("在籍中"),
+      exerciseList,
+    );
+    const member2 = new Member(
+      "jiroid",
+      "jiro",
+      "jiro@mail.com",
+      new ActivityStatus("在籍中"),
+      exerciseList,
+    );
+    const memberList = [member1, member2];
+    const pair = new Pair(id, name, memberList);
+
+    test("name", () => {
+      expect(pair.name).toEqual(name);
+    });
     // TODO: コンストラクタの定義
     /**
      * - [ ] ペア名
