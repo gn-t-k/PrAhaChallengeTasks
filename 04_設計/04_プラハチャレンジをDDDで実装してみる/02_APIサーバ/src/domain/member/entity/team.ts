8d24175e60eaf8b95d97fc02279c3697ee62a828
gn-t-k <gntk1995422@gmail.com>
2021-05-01 21:57:09 +0900

    チームクラス

--- 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/domain/member/entity/team.ts
+++ 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/domain/member/entity/team.ts
@@ -9,10 +9,27 @@ export class Team {
   private props: ITeam;
 
   constructor(id: string, name: string, pairList: Pair[]) {
+    if (!new RegExp("^[1-9]+$").test(name)) {
+      throw new Error("Team name can be set with numeric character.");
+    }
+    if (
+      !(
+        pairList
+          .map((pair) => pair.memberList.length)
+          .reduce((acc, cur) => acc + cur) >= 3
+      )
+    ) {
+      throw new Error("Team requires 3 or more members");
+    }
+
     this.props = { id, name, pairList };
   }
 
   public get name(): string {
     return this.props.name;
+  }
+
+  public get pairList(): Pair[] {
+    return this.props.pairList;
   }
 }
--- 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/test/domain/member/entity/team.test.ts
+++ 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/test/domain/member/entity/team.test.ts
@@ -16,11 +16,46 @@ describe("Team", () => {
     test("name", () => {
       expect(team.name).toEqual(name);
     });
+
+    test("pairList", () => {
+      expect(team.pairList).toEqual(pairList);
+    });
+  });
+
+  describe("バリデーション", () => {
+    const id = "123";
+
+    describe("数字以外のチーム名は設定できない", () => {
+      test("アルファベットを設定しようとした場合", () => {
+        const name = "a";
+        const pairList = [
+          makeDummyPair([makeDummyMember(), makeDummyMember()]),
+          makeDummyPair([makeDummyMember(), makeDummyMember()]),
+        ];
+
+        expect(() => {
+          const _team = new Team(id, name, pairList);
+        }).toThrowError("Team name can be set with numeric character.");
+      });
+    });
+
+    describe("参加者数は3名以上", () => {
+      test("合計の人数が2名以下のペアでチームを作成しようとした場合", () => {
+        const name = "1";
+        const pairList = [
+          makeDummyPair([makeDummyMember(), makeDummyMember()]),
+        ];
+
+        expect(() => {
+          const _team = new Team(id, name, pairList);
+        }).toThrowError("Team requires 3 or more members");
+      });
+    });
   });
 
   // TODO: コンストラクタの定義
   /**
-   * - [ ] チーム名は数字
-   * - [ ] 参加者数は3名以上
+   * - [x] チーム名は数字
+   * - [x] 参加者数は3名以上
    */
 });
