c965b821debecc0275abe3f026816f1c401a4c77
gn-t-k <gntk1995422@gmail.com>
2021-05-25 23:44:58 +0900

    不要なファイル削除

--- 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/infra/in-memory/repository/team-repository.ts
+++
@@ -1,8 +0,0 @@
-import { ITeamRepository, ITeamStructure } from "domain/team/team-repository";
-
-export class TeamRepository implements ITeamRepository {
-  // eslint-disable-next-line class-methods-use-this
-  public getAll(): Promise<ITeamStructure> {
-    return Promise.resolve({ teamList: [], independentMemberList: [] });
-  }
-}
