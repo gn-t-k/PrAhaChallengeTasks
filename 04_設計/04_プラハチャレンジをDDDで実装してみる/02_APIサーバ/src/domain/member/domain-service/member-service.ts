133d9a0e8817e10a8c62e23eb96f09fabd563a01
gn-t-k <gntk1995422@gmail.com>
2021-05-31 22:26:25 +0900

    wip

--- 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/domain/member/domain-service/member-service.ts
+++ 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/domain/member/domain-service/member-service.ts
@@ -1,6 +1,14 @@
+import {
+  ActivityStatus,
+  activityStatusValue,
+} from "../value-object/activity-status";
 import { Member } from "domain/member/entity/member";
 import { IMemberRepository } from "domain/member/member-repository-interface";
 
+interface IMemberFactoryProps {
+  name: string;
+  email: string;
+}
 export class MemberService {
   private readonly repository: IMemberRepository;
 
@@ -12,5 +20,14 @@ export class MemberService {
     const memberList = await this.repository.getAll();
 
     return memberList.some((m) => m.equals(member) || m.email === member.email);
+  }
+
+  public static factory(props: IMemberFactoryProps): Member {
+    const { name, email } = props;
+    const activityStatus = ActivityStatus.create({
+      status: activityStatusValue.active,
+    });
+
+    return Member.create({ name, email, activityStatus });
   }
 }
--- 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/usecase/register-member.ts
+++ 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/usecase/register-member.ts
@@ -1,6 +1,10 @@
 import { MemberService } from "domain/member/domain-service/member-service";
-import { Member } from "domain/member/entity/member";
 import { IMemberRepository } from "domain/member/member-repository-interface";
+
+export interface IRegisterMemberProps {
+  name: string;
+  email: string;
+}
 
 export class RegisterMember {
   private readonly repository: IMemberRepository;
@@ -11,9 +15,12 @@ export class RegisterMember {
     this.memberService = new MemberService(repository);
   }
 
-  public async execute(member: Member): Promise<void> {
-    const isExists = await this.memberService.isExist(member);
-    if (isExists) {
+  public async execute(props: IRegisterMemberProps): Promise<void> {
+    const { name, email } = props;
+    const member = MemberService.factory({ name, email });
+    const isExist = await this.memberService.isExist(member);
+
+    if (isExist) {
       throw new Error("Member already exists");
     }
 
