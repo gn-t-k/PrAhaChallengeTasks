9264f033da1b40b9afb8787302cc1bf33cf38b09
gn-t-k <gntk1995422@gmail.com>
2021-06-01 20:44:18 +0900

    wip

---
+++ 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/infra/db/repository/member-repository.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from ".prisma/client";
+import { Member } from "domain/member/entity/member";
+import { IMemberRepository } from "domain/member/member-repository-interface";
+import { ActivityStatus } from "domain/member/value-object/activity-status";
+import { Identifier } from "domain/shared/identifier";
+import { Context } from "infra/db/context";
+
+export class MemberRepository implements IMemberRepository {
+  private readonly prisma: PrismaClient;
+
+  constructor(context: Context) {
+    this.prisma = context.prisma;
+  }
+
+  public async register(member: Member): Promise<void> {
+    const id = member.id.value;
+    const { name, email } = member;
+    const activityStatus = member.status.value;
+
+    await this.prisma.member.create({
+      data: { id, name, email, activityStatus },
+    });
+  }
+
+  public async getAll(): Promise<Member[]> {
+    const memberDataList = await this.prisma.member.findMany();
+
+    return memberDataList.map((memberData) => {
+      const id = new Identifier(memberData.id);
+      const { name, email } = memberData;
+      const activityStatus = ActivityStatus.create({
+        status: memberData.activityStatus,
+      });
+
+      return Member.rebuild(id, { name, email, activityStatus });
+    });
+  }
+}
