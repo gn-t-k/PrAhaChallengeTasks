805a49ac77bab67b8c80df6e36ef96daff9981ac
gn-t-k <gntk1995422@gmail.com>
2021-06-10 08:56:26 +0900

    wip

--- 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/infra/db/query-service/get-member-by-team-name-query-service.ts
+++ 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/infra/db/query-service/get-member-by-team-name-query-service.ts
@@ -1,4 +1,4 @@
-import { MemberOnPair, PrismaClient } from "@prisma/client";
+import { Member, MemberOnPair, Pair, PrismaClient, Team } from "@prisma/client";
 import { Context } from "infra/db/context";
 import {
   GetMemberByTeamNameDTO,
@@ -16,56 +16,16 @@ export class GetMemberByTeamNameQueryService
   public execute = async (
     teamName: string,
   ): Promise<GetMemberByTeamNameDTO> => {
-    const teamData = await this.prisma.team.findUnique({
-      where: {
-        name: teamName,
-      },
-    });
-
-    if (teamData === null) {
-      throw new Error(`Team named ${teamName} is not exists`);
-    }
-
-    const pairDataList = await this.prisma.pair.findMany({
-      where: {
-        teamId: teamData.id,
-      },
-    });
-
-    if (!pairDataList) {
-      throw new Error(`Team ${teamName} has no pair`);
-    }
-
-    const memberOnPairPromiseList = pairDataList.map((pairData) =>
-      this.prisma.memberOnPair.findMany({
-        where: {
-          pairId: pairData.id,
-        },
-      }),
+    const teamData = await this.getTeamData(teamName);
+    const pairDataList = await this.getPairDataList(teamData.id);
+    const memberOnPairDataList = await this.getMemberOnPairDataList(
+      pairDataList,
     );
-    const memberOnPairData = await Promise.all(memberOnPairPromiseList);
-
-    const flattedMemberOnPairData = memberOnPairData.reduce(
-      (acc: MemberOnPair[], cur: MemberOnPair[]) => acc.concat(cur),
-      [],
-    );
-    const memberPromiseList = flattedMemberOnPairData.map((mop) =>
-      this.prisma.member.findUnique({
-        where: {
-          id: mop.memberId,
-        },
-      }),
-    );
-    const memberDataList = await Promise.all(memberPromiseList);
+    const memberDataList = await this.getMemberDataList(memberOnPairDataList);
 
     return memberDataList.map((memberData) => {
-      if (memberData === null) {
-        // コンパイルエラー避けのエラー。ダサい。
-        throw new Error();
-      }
-
       const { id, name, email, activityStatus } = memberData;
-      const pairID = flattedMemberOnPairData.find((mop) => mop.memberId === id)
+      const pairID = memberOnPairDataList.find((mop) => mop.memberId === id)
         ?.pairId;
       const teamID = teamData.id;
 
@@ -82,6 +42,69 @@ export class GetMemberByTeamNameQueryService
         pairID,
         teamID,
       };
+    });
+  };
+
+  private getTeamData = async (teamName: string): Promise<Team> => {
+    const teamData = await this.prisma.team.findUnique({
+      where: {
+        name: teamName,
+      },
+    });
+
+    if (teamData === null) {
+      throw new Error(`Team named ${teamName} is not exists`);
+    }
+
+    return teamData;
+  };
+
+  private getPairDataList = async (teamId: string): Promise<Pair[]> => {
+    const pairDataList = await this.prisma.pair.findMany({
+      where: {
+        teamId,
+      },
+    });
+
+    if (pairDataList.length === 0) {
+      throw new Error(`Team has no pair`);
+    }
+
+    return pairDataList;
+  };
+
+  private getMemberOnPairDataList = async (pairDataList: Pair[]) => {
+    const pairIDDataList = pairDataList.map((p) => p.id);
+    const memberOnPairDataList = await this.prisma.memberOnPair.findMany({
+      where: {
+        pairId: {
+          in: pairIDDataList,
+        },
+      },
+    });
+
+    return memberOnPairDataList;
+  };
+
+  private getMemberDataList = async (
+    memberOnPairDataList: MemberOnPair[],
+  ): Promise<Member[]> => {
+    const memberIDDataList = memberOnPairDataList.map((mop) => mop.memberId);
+    const memberDataList = await this.prisma.member.findMany({
+      where: {
+        id: {
+          in: memberIDDataList,
+        },
+      },
+    });
+
+    return memberDataList.map((memberData) => {
+      if (memberData === null) {
+        // コンパイルエラー避けのエラー。ダサい。
+        throw new Error();
+      }
+
+      return memberData;
     });
   };
 }
--- 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/usecase/query-service-interface/get-member-by-team-name-query-service.ts
+++ 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/usecase/query-service-interface/get-member-by-team-name-query-service.ts
@@ -1,6 +1,11 @@
 import { MemberDTO } from "usecase/query-service-interface/entity-dto/member-dto";
 
-export type GetMemberByTeamNameDTO = MemberDTO[];
+interface OneOfGetMemberByTeamNameDTO extends MemberDTO {
+  pairID: string;
+  teamID: string;
+}
+
+export type GetMemberByTeamNameDTO = OneOfGetMemberByTeamNameDTO[];
 
 export interface IGetMemberByTeamNameQueryService {
   execute(teamName: string): Promise<GetMemberByTeamNameDTO>;
