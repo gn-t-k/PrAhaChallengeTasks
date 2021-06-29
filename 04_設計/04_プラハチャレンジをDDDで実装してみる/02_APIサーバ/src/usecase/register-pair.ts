5b5d98ff26b54afaa0cc386a2e57a005d7c00966
gn-t-k <gntk1995422@gmail.com>
2021-06-30 08:06:04 +0900

    wip

--- 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/usecase/register-pair.ts
+++ 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/usecase/register-pair.ts
@@ -1,16 +1,25 @@
+import { Identifier } from "domain/__shared__/identifier";
+import { IMemberRepository } from "domain/member/member-repository-interface";
+import { IsMemberExistsInTeamService } from "domain/team/service/is-member-exists-in-team-service";
 import { IsPairExistsService } from "domain/team/service/is-pair-exists-service";
 import { ITeamRepository } from "domain/team/team-repository-interface";
 
 export class RegisterPair {
-  private readonly repository: ITeamRepository;
+  private readonly teamRepository: ITeamRepository;
+  private readonly memberRepository: IMemberRepository;
   private readonly isPairExistsService: IsPairExistsService;
+  private readonly isMemberExistsInTeamService: IsMemberExistsInTeamService;
 
   public constructor(
-    repository: ITeamRepository,
+    teamRepository: ITeamRepository,
+    memberRepository: IMemberRepository,
     isPairExistsService: IsPairExistsService,
+    isMemberExistsInTeamService: IsMemberExistsInTeamService,
   ) {
-    this.repository = repository;
+    this.teamRepository = teamRepository;
+    this.memberRepository = memberRepository;
     this.isPairExistsService = isPairExistsService;
+    this.isMemberExistsInTeamService = isMemberExistsInTeamService;
   }
 
   public execute = async (
@@ -27,7 +36,30 @@ export class RegisterPair {
       throw new Error(`Pair ${pairName} is already exists in specified team`);
     }
 
-    // TODO: 参加者が存在するか、ペアに所属していないかチェック
+    const getMemberPromiseList = memberIDList.map((memberID) =>
+      this.memberRepository.getByID(new Identifier(memberID)),
+    );
+    const memberList = await Promise.all(getMemberPromiseList);
+    const isAllMemberExist = memberList.every((member) => member !== null);
+
+    if (!isAllMemberExist) {
+      throw new Error("Some or all member is not exists");
+    }
+
+    const isMemberExistsInTeamPromiseList = memberIDList.map((memberID) =>
+      this.isMemberExistsInTeamService.execute(new Identifier(memberID)),
+    );
+    const isMemberExistsInTeamList = await Promise.all(
+      isMemberExistsInTeamPromiseList,
+    );
+    const isAllMemberFree = isMemberExistsInTeamList.every(
+      (isMemberExistsInTeam) => isMemberExistsInTeam === false,
+    );
+
+    if (!isAllMemberFree) {
+      throw new Error("Some or all member is already exists in team");
+    }
+
     // TODO: Pairオブジェクトの作成、登録
   };
 }
