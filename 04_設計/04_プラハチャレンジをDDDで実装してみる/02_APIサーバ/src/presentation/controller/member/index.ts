afca6ef612119061024d3d81cc6b1666e5a77f7d
gn-t-k <gntk1995422@gmail.com>
2021-07-24 19:31:50 +0900

    不要なファイル削除

--- 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/presentation/controller/member/index.ts
+++ 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/presentation/controller/member/index.ts
@@ -2,9 +2,5 @@ export { ChangeActivityStatusController } from "presentation/controller/member/c
 export { ChangeProgressStatusToNextController } from "presentation/controller/member/change-progress-status-to-next-controller";
 export { ChangeProgressStatusToPreviousController } from "presentation/controller/member/change-progress-status-to-previous-controller";
 export { DeleteMemberController } from "presentation/controller/member/delete-member-controller";
-export { GetAllMemberController } from "presentation/controller/member/get-all-member-controller";
 export { GetMemberController } from "presentation/controller/member/get-member-controller";
-export { GetMemberByExerciseAndProgressController } from "presentation/controller/member/get-member-by-exercise-and-progress-controller";
-export { GetMemberByPairNameController } from "presentation/controller/member/get-member-by-pair-name-controller";
-export { GetMemberByTeamNameController } from "presentation/controller/member/get-member-by-team-name-controller";
 export { RegisterMemberController } from "presentation/controller/member/register-member-controller";
