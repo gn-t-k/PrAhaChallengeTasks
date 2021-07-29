894d011da3035dc0613d4e6b752bd2b79fe013af
gn-t-k <gntk1995422@gmail.com>
2021-07-29 09:14:18 +0900

    wip

--- 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/presentation/implementation/express/service/member-route-factory.ts
+++ 04_設計/04_プラハチャレンジをDDDで実装してみる/02_APIサーバ/src/presentation/implementation/express/service/member-route-factory.ts
@@ -1,19 +1,35 @@
 import { Router } from "express";
+import { IExerciseRepository } from "domain/exercise/exercise-repository-interface";
+import { IMemberRepository } from "domain/member/member-repository-interface";
+import { ChangeActivityStatusService } from "domain/member/service/change-activity-status-service";
+import { DeleteMemberService } from "domain/member/service/delete-member-service";
 import { IsMemberExistService } from "domain/member/service/is-member-exist-service";
+import { IProgressRepository } from "domain/progress/progress-repository-interface";
 import { IsProgressExistService } from "domain/progress/service/is-progress-exist-service";
+import { ITeamRepository } from "domain/team/team-repository-interface";
 import { Context } from "infrastructure/db/context";
 import * as QueryService from "infrastructure/db/query-service";
 import * as Repository from "infrastructure/db/repository";
-import * as Controller from "presentation/controller";
+import * as MemberController from "presentation/controller/member";
 import { RouteRegistrarsFactory } from "presentation/implementation/express/service/route-registrars-factory";
 import { MemberRoute } from "presentation/route";
 import * as Usecase from "usecase";
 
 export class MemberRouteFactory {
+  private readonly memberRepository: IMemberRepository;
+  private readonly exerciseRepository: IExerciseRepository;
+  private readonly progressRepository: IProgressRepository;
+  private readonly teamRepository: ITeamRepository;
+
   public constructor(
     private readonly router: Router,
     private readonly context: Context,
-  ) {}
+  ) {
+    this.memberRepository = new Repository.MemberRepository(context);
+    this.exerciseRepository = new Repository.ExerciseRepository(context);
+    this.progressRepository = new Repository.ProgressRepository(context);
+    this.teamRepository = new Repository.TeamRepository(context);
+  }
 
   public execute = (): MemberRoute => {
     const {
@@ -25,9 +41,26 @@ export class MemberRouteFactory {
 
     const getMemberController = this.generateGetMemberController();
     const registerMemberController = this.generateRegisterMemberController();
+    const changeActivityStatusController = this.generateChangeActivityStatusController();
+    const changeProgressStatusToNextController = this.generateChangeProgressStatusToNextController();
+    const changeProgressStatusToPreviousController = this.generateChangeProgressStatusToPreviousController();
+    const deleteMemberController = this.generateDeleteMemberController();
+
+    return new MemberRoute(
+      getRouteRegistrar,
+      postRouteRegistrar,
+      putRouteRegistrar,
+      deleteRouteRegistrar,
+      getMemberController,
+      registerMemberController,
+      changeActivityStatusController,
+      changeProgressStatusToNextController,
+      changeProgressStatusToPreviousController,
+      deleteMemberController,
+    );
   };
 
-  private generateGetMemberController = (): Controller.Member.GetMemberController => {
+  private generateGetMemberController = (): MemberController.GetMemberController => {
     const getAllMemberQueryService = new QueryService.GetAllMemberQueryService(
       this.context,
     );
@@ -56,7 +89,7 @@ export class MemberRouteFactory {
       getMemberByExerciseAndProgressQueryService,
     );
 
-    return new Controller.Member.GetMemberController(
+    return new MemberController.GetMemberController(
       getAllMemberUsecase,
       getMemberByTeamNameUsecase,
       getMemberByPairNameUsecase,
@@ -64,25 +97,80 @@ export class MemberRouteFactory {
     );
   };
 
-  private generateRegisterMemberController = (): Controller.Member.RegisterMemberController => {
-    const memberRepository = new Repository.MemberRepository(this.context);
-    const exerciseRepository = new Repository.ExerciseRepository(this.context);
-    const progressRepository = new Repository.ProgressRepository(this.context);
-    const isMemberExistService = new IsMemberExistService(memberRepository);
+  private generateRegisterMemberController = (): MemberController.RegisterMemberController => {
+    const isMemberExistService = new IsMemberExistService(
+      this.memberRepository,
+    );
     const isProgressExistService = new IsProgressExistService(
-      progressRepository,
+      this.progressRepository,
     );
 
     const registerMemberUsecase = new Usecase.RegisterMember({
-      memberRepository,
-      exerciseRepository,
-      progressRepository,
+      memberRepository: this.memberRepository,
+      exerciseRepository: this.exerciseRepository,
+      progressRepository: this.progressRepository,
       isMemberExistService,
       isProgressExistService,
     });
 
-    return new Controller.Member.RegisterMemberController(
-      registerMemberUsecase,
+    return new MemberController.RegisterMemberController(registerMemberUsecase);
+  };
+
+  private generateChangeActivityStatusController = (): MemberController.ChangeActivityStatusController => {
+    const changeActivityStatusService = new ChangeActivityStatusService(
+      this.memberRepository,
+      this.teamRepository,
     );
+    const changeActivityStatusToActiveUsecase = new Usecase.ChangeActivityStatusToActive(
+      this.memberRepository,
+      changeActivityStatusService,
+    );
+    const changeActivityStatusToInRecessUsecase = new Usecase.ChangeActivityStatusToInRecess(
+      this.memberRepository,
+      changeActivityStatusService,
+    );
+    const changeActivityStatusToLeftUsecase = new Usecase.ChangeActivityStatusToLeft(
+      this.memberRepository,
+      changeActivityStatusService,
+    );
+
+    return new MemberController.ChangeActivityStatusController(
+      changeActivityStatusToActiveUsecase,
+      changeActivityStatusToInRecessUsecase,
+      changeActivityStatusToLeftUsecase,
+    );
+  };
+
+  private generateChangeProgressStatusToNextController = (): MemberController.ChangeProgressStatusToNextController => {
+    const changeProgressStatusNextUsecase = new Usecase.ChangeProgressStatusNext(
+      this.progressRepository,
+    );
+
+    return new MemberController.ChangeProgressStatusToNextController(
+      changeProgressStatusNextUsecase,
+    );
+  };
+
+  private generateChangeProgressStatusToPreviousController = (): MemberController.ChangeProgressStatusToPreviousController => {
+    const changeProgressStatusPreviousUsecase = new Usecase.ChangeProgressStatusPrevious(
+      this.progressRepository,
+    );
+
+    return new MemberController.ChangeProgressStatusToPreviousController(
+      changeProgressStatusPreviousUsecase,
+    );
+  };
+
+  private generateDeleteMemberController = (): MemberController.DeleteMemberController => {
+    const deleteMemberService = new DeleteMemberService(
+      this.memberRepository,
+      this.teamRepository,
+    );
+    const deleteMemberUsecase = new Usecase.DeleteMember(
+      this.memberRepository,
+      deleteMemberService,
+    );
+
+    return new MemberController.DeleteMemberController(deleteMemberUsecase);
   };
 }
