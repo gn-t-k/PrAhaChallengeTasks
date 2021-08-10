e5d30bcd6d8b9eac93d8fb2f698044b7ba6eff26
gn-t-k <gntk1995422@gmail.com>
2021-08-11 08:37:31 +0900

    レスポンシブ対応

--- 05_フロントエンド/02_適切にコンポーネントを分割して1ページ作ってみよう/app/src/components/molecule/navigation/index.tsx
+++ 05_フロントエンド/02_適切にコンポーネントを分割して1ページ作ってみよう/app/src/components/molecule/navigation/index.tsx
@@ -7,18 +7,25 @@ type Props = {
 };
 
 const Navigation = (props: Props): JSX.Element => (
-  <ul className={styles.list}>
-    {props.children.map((child) => {
-      const id = child[0];
-      const component = child[1];
+  <>
+    <ul className={styles.list}>
+      {props.children.map((child) => {
+        const id = child[0];
+        const component = child[1];
 
-      return (
-        <li key={id} className={styles.item}>
-          {component}
-        </li>
-      );
-    })}
-  </ul>
+        return (
+          <li key={id} className={styles.item}>
+            {component}
+          </li>
+        );
+      })}
+    </ul>
+    <span className={styles.button}>
+      <svg viewBox="0 0 24 24">
+        <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+      </svg>
+    </span>
+  </>
 );
 
 export default Navigation;
